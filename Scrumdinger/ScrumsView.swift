06970dee4f4e62a91555076300266c863cab1e19
shamitomita <shamitomita@gmail.com>
2022-09-20 16:54:55 -0400

    Use the edit view to create a new scrum

--- Scrumdinger/ScrumsView.swift
+++ Scrumdinger/ScrumsView.swift
@@ -6,7 +6,8 @@ import SwiftUI
 
 struct ScrumsView: View {
     @Binding var scrums: [DailyScrum]
-    @State private var isPResentingNewScrumView = false
+    @State private var isPresentingNewScrumView = false
+    @State private var newScrumData = DailyScrum.Data()
     var body: some View {
         List {
             ForEach($scrums) { $scrum in
@@ -18,10 +19,33 @@ struct ScrumsView: View {
         }
         .navigationTitle("Daily Scrums")
         .toolbar {
-            Button(action: {}) {
+            Button(action: {
+                isPresentingNewScrumView = true
+            }) {
                 Image(systemName: "plus")
             }
             .accessibilityLabel("New Scrum")
+        }
+        .sheet(isPresented: $isPresentingNewScrumView){
+            NavigationView{
+                DetailEditView(data: $newScrumData)
+                    .toolbar{
+                        ToolbarItem(placement: .cancellationAction){
+                            Button("Dismiss"){
+                                isPresentingNewScrumView = false
+                                newScrumData = DailyScrum.Data()
+                            }
+                        }
+                        ToolbarItem(placement: .confirmationAction){
+                            Button("Add"){
+                                let newScrum = DailyScrum(data: newScrumData)
+                                scrums.append(newScrum)
+                                isPresentingNewScrumView = false
+                                newScrumData = DailyScrum.Data()
+                            }
+                        }
+                    }
+            }
         }
     }
 }
